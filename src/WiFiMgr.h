53b574bb6a85697aeb177cfd4ee501bb816c467f
WasiliySoft-Home <wasiliysoft@ya.ru>
2025-11-03 20:02:24 +0500

    fix freeze on fail connected to WiFi

--- src/WiFiMgr.h
+++ src/WiFiMgr.h
@@ -5,12 +5,17 @@
 #include <ESP8266mDNS.h>
 #include <WiFiManager.h>
 
+// Таймаут портала конфигурации:
+// нужно чтобы не зависала в режиме настройки из-за пропадания эл. питания.
+// https://github.com/tzapu/WiFiManager?tab=readme-ov-file#configuration-portal-timeout
+#define CONFIG_PORTAL_TIMEOUT_SEC 180
+
 class WiFiMgr {
 public:
   WiFiMgr() {}
-  void begin(const char* apName) {
+  void begin(const char *apName) {
     WiFiManager wifiManager;
-
+    wifiManager.setConfigPortalTimeout(CONFIG_PORTAL_TIMEOUT_SEC);
     // Авто-подключение с точкой доступа для настройки
     if (!wifiManager.autoConnect(apName)) {
       Serial.println("Failed to connect, restarting...");
